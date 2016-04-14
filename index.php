4e0ea92e844283514792c0e768a4c38a24775e39
Mutanoball <allkaline@yandex.ru>
2016-04-14 08:10:12 +0000

    1st commit@

--- index.php
+++ index.php
@@ -1,1 +1,39 @@
-blablabla
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>Юридическая компания LEX FORI</title>
+        <script src='https://code.jquery.com/jquery-2.1.4.min.js'></script>
+        <link rel="stylesheet" type="text/css" href="main.css">
+    </head>
+    
+    <body>
+		<div class="head">
+			<div class="logo">
+                <div class="lex">
+                    Lex Fori
+                </div>
+                <div class="yk">
+                    ЮРИДИЧЕСКАЯ КОМПАНИЯ
+                </div>
+            </div>
+
+		</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	</body>
+</html>
