ce0311ad3c2314593a254efb40f1c9818eeb77c2
Mutanoball <allkaline@yandex.ru>
2016-04-15 06:59:43 +0000

    1st commit

--- index.php
+++ index.php
@@ -49,6 +49,28 @@
                 вопрос-ответ
             </div>
         </div>
+        <div class="content news">
+            <div class="title">
+                новости
+            </div>
+            <div class="inner">
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci. Aenean nec lorem. In porttitor. Donec laoreet nonummy augue.
+                Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy. Fusce aliquet pede non pede. Suspendisse dapibus lorem pellentesque magna. Integer nulla. Donec blandit feugiat ligula. Donec hendrerit, felis et imperdiet euismod, purus ipsum pretium metus, in lacinia nulla nisl eget sapien. Donec ut est in lectus consequat consequat. Etiam eget dui. Aliquam erat volutpat. Sed at lorem in nunc porta tristique.
+            </div>
+        </div>
+        <div class="ground">
+            <div class "l_col">
+                <div class="logo">
+                    <div class="lex">
+                        Lex Fori
+                    </div>
+                    <div class="yk">
+                        ЮРИДИЧЕСКАЯ КОМПАНИЯ
+                    </div>
+                </div>
+            </div>
+        </div>
+
 
 
 
